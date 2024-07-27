51bbc10a475e6f1bbc0522bf91033d86975e7c11
Vaibhaviitian <aryanvaibhav74@gmail.com>
2024-07-27 13:23:31 +0530

    Giving flex for footer

--- src/components/users/Footer.jsx
+++ src/components/users/Footer.jsx
@@ -4,85 +4,85 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer className="bg-white border-y mt-5">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
-          
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Resources
-              </h2>
-              <ul className="text-gray-500 font-medium">
-                <li className="mb-4">
-                  <Link to="/home" className="hover:underline">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/about" className="hover:underline">
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Follow us
-              </h2>
-              <ul className="text-gray-500 font-medium">
-                <li className="mb-4">
-                  <a
-                    href="https://github.com/Vaibhaviitian"
-                    className="hover:underline"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/vaibhav-aryan-00650b287/"
-                    className="hover:underline"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    LinkedIn
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Legal
-              </h2>
-              <ul className="text-gray-500 font-medium">
-                <li className="mb-4">
-                  <Link to="/privacy-policy" className="hover:underline">
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#" className="hover:underline">
-                    Terms &amp; Conditions
-                  </Link>
-                </li>
-              </ul>
-            </div>
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div className="flex flex-col items-center">
+        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+              Resources
+            </h2>
+            <ul className="text-gray-500 font-medium">
+              <li className="mb-4">
+                <Link to="/home" className="hover:underline">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="hover:underline">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+              Follow us
+            </h2>
+            <ul className="text-gray-500 font-medium">
+              <li className="mb-4">
+                <a
+                  href="https://github.com/Vaibhaviitian"
+                  className="hover:underline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/vaibhav-aryan-00650b287/"
+                  className="hover:underline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+              Legal
+            </h2>
+            <ul className="text-gray-500 font-medium">
+              <li className="mb-4">
+                <Link to="/privacy-policy" className="hover:underline">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline">
+                  Terms &amp; Conditions
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center  sm:justify-between">
-          <span className="text-sm text-gray-500 flex items-center sm:text-center">
-            <strong>© 2024
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 text-center">
+          <strong>© 2024
             <a href="/" className="hover:underline">
               Travel Track
             </a>
-            . All Rights Reserved.</strong>
-            
-          </span>
-        </div>
+            . All Rights Reserved.
+          </strong>
+        </span>
       </div>
-    </footer>
+    </div>
+  </footer>
+  
   );
 }

a4279798fdb82f380314c40e587c3f66a3b48d86
Vaibhaviitian <aryanvaibhav74@gmail.com>
2024-07-30 15:45:29 +0530

    Solving lF error

--- src/components/community/LostandFound.jsx
+++ src/components/community/LostandFound.jsx
@@ -5,7 +5,7 @@ import BlogCard from "./BlogCard.jsx";
 const LostandFound = () => {
   const [lostposts, setlostposts] = useState([]);
   const [error, setError] = useState(null);
-  let id = localStorage.getItem("id");
+  let id = localStorage.getItem('id');
   id = String(id);
 
   const getdata = async () => {
@@ -35,30 +35,28 @@ const LostandFound = () => {
 
   return (
     <>
-      {lostposts.length > 0 ? (
+      {error ? (
+        <div>Error: {error.message}</div>
+      ) : lostposts.length > 0 ? (
         lostposts.map((post) => (
-          <BlogCard
-            key={post?._id}
-            id={post?._id}
-            islost={post?.laf}
-            title={post?.title}
-            message={post?.message}
-            photo={post?.photo}
-            creatingtime={post?.createdAt}
-            user={post?.user?.username}
-            isuser={id === String(post?.user?._id)}
-          />
+          post?.laf ? (
+            <BlogCard
+              key={post?._id}
+              id={post?._id}
+              islost={post?.laf}
+              title={post?.title}
+              message={post?.message}
+              photo={post?.photo}
+              creatingtime={post?.createdAt}
+              user={post?.user?.username}
+              isuser={id === String(post?.user?._id)}
+            />
+          ) : (
+            <></>
+          )
         ))
       ) : (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
-          <div
-            className="spinner-border text-orange-500"
-            role="status"
-            style={{ width: "5rem", height: "5rem" }}
-          >
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        </div>
+        <></>
       )}
     </>
   );
